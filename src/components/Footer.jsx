f02c68f7f52b776d7c430289c70c80517ad4e678
SW49GO <gohard.so49@gmail.com>
2023-08-28 11:15:00 +0200

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import '../styles/footer.css'
+
+function Footer(){
+    return <footer className="footer">
+                <div className='footer_image'>
+                    <img className="imageFooter" src="../assets/logoFooter.svg" alt="Logo Kasa"/>
+                </div>
+                <p>&copy; 2020 Kasa. All rights reserved</p>
+            </footer>
+}
+
+export default Footer
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client"
 import './styles/index.css'
 import Routage from './components/Router'
+import Footer from './components/Footer'
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <React.StrictMode>
         <Routage/>
+        <Footer/>
     </React.StrictMode>
 )
