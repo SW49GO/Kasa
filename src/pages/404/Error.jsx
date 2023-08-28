0e77e088c80a7d4f6d8ee7cc21d8bdbe8353f222
SW49GO <gohard.so49@gmail.com>
2023-08-28 11:05:39 +0200

    Page Error

--- src/pages/404/Error.jsx
+++ src/pages/404/Error.jsx
@@ -1,6 +1,11 @@
+import { Link } from "react-router-dom"
 import '../../styles/error.css'
 function Error(){
-    return <div></div>
+    return <div className="error">
+                <p className="error_404">404</p>
+                <p className="error_Txt">oups! La page que vous demandez n'existe pas.</p>
+                <Link className="error_Link" to="/">Retourner sur la page d'accueil</Link>
+           </div>
 }
 
 export default Error
