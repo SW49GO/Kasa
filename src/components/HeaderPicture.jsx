78cd7dffcc1a27be924f76b8a09462d37dde1557
SW49GO <gohard.so49@gmail.com>
2023-08-30 14:10:50 +0200

    Change Height Image between the 2 pages

--- src/components/HeaderPicture.jsx
+++ src/components/HeaderPicture.jsx
@@ -8,12 +8,17 @@ function HeaderPicture(props){
     console.log('homePage:', homePage)
     const location = useLocation();
     const pathSrc= location.pathname==="/"?'./assets/mer.svg':'./assets/montagne.svg';
+    const isAboutPage = location.pathname === '/About';
 
-    
-    return <div className="homePage_image">
-                <img className="image" src={pathSrc} alt="Mer ou Montagne"/>
-                {homePage &&(<div className="homePage_title"><p>Chez vous,</p><p> partout et ailleurs</p></div>)}
-           </div>
+    return <div className={`homePage_image ${isAboutPage ? 'aboutPage_image' : ''}`}>
+    <img className="image" src={pathSrc} alt="Mer ou Montagne" />
+    {homePage && (
+      <div className="homePage_title">
+        <p>Chez vous,</p>
+        <p> partout et ailleurs</p>
+      </div>
+    )}
+  </div>
 }
 
 export default HeaderPicture
