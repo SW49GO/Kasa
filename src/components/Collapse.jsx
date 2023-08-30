05796b9f052e7ced62fa1c144258769060c7a8f3
SW49GO <gohard.so49@gmail.com>
2023-08-30 15:03:55 +0200

    About CSS

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,14 +1,17 @@
 import '../styles/about.css'
 import useDropdown from '../utils/hooks/useDropdown';
+import { useLocation } from 'react-router-dom';
 
 function Collapse(props) {
   const title = props.title;
   const description = props.description;
   const [isOpen, toggleDropdown] = useDropdown(false);
+  const location = useLocation();
+  const isAboutPage = location.pathname === '/About';
 
   return (
     <div className="dropdown">
-      <div className="dropdown_menu">
+      <div className={`dropdown_menu ${isAboutPage ? 'dropdown_margin' : ''}`}>
         <p>{title}</p>
         {isOpen ? (
           <i className="fa-solid fa-chevron-up fa-rotate-180" onClick={toggleDropdown}></i>
@@ -16,7 +19,7 @@ function Collapse(props) {
           <i className="fa-solid fa-chevron-up fa-rotate-360" onClick={toggleDropdown}></i>
         )}
       </div>
-      {isOpen && <div className="dropdown_detail">{description}</div>}
+      {isOpen && <div className={`dropdown_detail ${isAboutPage ? 'dropdown_margin' : ''}`}>{description}</div>}
     </div>
   );
 }
